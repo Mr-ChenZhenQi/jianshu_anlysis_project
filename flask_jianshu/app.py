01a9a707417c0e7ad787d16fafa3d00d50eeef99
zhangsan <zhangsan@qq.com>
2020-04-08 12:16:05 +0800

    使用递归的方法爬取被收集用户所有动态

--- flask_jianshu/app.py
+++ flask_jianshu/app.py
@@ -35,39 +35,53 @@ BASE_HEADERS = {
     'Referer': 'http://www.jianshu.com',
 }
 
+timeline = {
+    'comment_note': [],
+    'like_note': [],
+    'reward_note': [],
+    'share_note': [],
+    'like_user': [],
+    'like_collection': [],
+    'like_comment': [],
+    'like_notebook': [],
+}
 
-def jianshu_timeline(slug):
+join_jianshu_time = ''
+
+def jianshu_timeline(slug,maxid,page):
+    global join_jianshu_time
     AJAX_HEADERS = {"Referer": f"https//:www.jianshu.com/u/{slug}",
                     "X-PJAX": "true"}
     headers = dict(BASE_HEADERS, **AJAX_HEADERS)
 
-    timeline = {
-        'comment_note': [],
-        'like_note': [],
-        'reward_note': [],
-        'share_note': [],
-        'like_user': [],
-        'like_collection': [],
-        'like_comment': [],
-        'like_notebook': [],
-    }
-    url=f'http://www.jianshu.com/users/{slug}/timeline'
-    # slug = '8b23f6864f5d'
+    print(f'爬取第{page}页动态')
+
+    if maxid==None:
+        url = f'http://www.jianshu.com/users/{slug}/timeline'
+    else:
+        url=f'https://www.jianshu.com/users/{slug}/timeline?max_id={maxid}&page={page}'
+
     respont= requests.get(url=url,headers=headers)
     tree=etree.HTML(respont.text)
     lis=tree.xpath('.//li')
+    last_li_id=lis[-1].xpath('@id')[0].replace('feed-','')
+    maxid=int(last_li_id)-1
     if lis:
         for li in lis:
             type=li.xpath('.//@data-type')[0]
             time=li.xpath('.//@data-datetime')[0].replace('T','').replace('+08:00','')
+            if type =='join_jianshu':
+                join_jianshu_time=time
+                return
             timeline[type].append(time)
         pprint(timeline)
-    pass
+
+    jianshu_timeline(slug,maxid,page+1)
 
 
 
 if __name__ == '__main__':
     slug = '8b23f6864f5d'
-    jianshu_timeline(slug)
+    jianshu_timeline(slug,None,1)
     # app.run()
 
